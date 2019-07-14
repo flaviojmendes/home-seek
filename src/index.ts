3f20f7e1a9915dc06c863e58c7f58700aec626d4
Flavio Mendes <flaviojmendes@gmail.com>
2019-07-14 15:51:52 +0100

    first stable version

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,6 @@ class HomeSeek extends Command {
   async run() {
     const {args, flags} = this.parse(HomeSeek)
     let daftService = new DaftService();
-
     const url = flags.name || 'https://www.daft.ie/dublin-city/residential-property-for-rent/blackrock,cabinteely,carpenterstown,carrickmines,churchtown,dalkey,deans-grange,donnybrook,dun-laoghaire,dundrum,foxrock,goatstown,killiney,leopardstown,monkstown,sandycove,sandyford,stepaside,stillorgan/?mxp=2000&mnb=2&mnbt=2&advanced=1&cc_id=ct1&a_id%5B0%5D=188&a_id%5B1%5D=194&a_id%5B2%5D=196&a_id%5B3%5D=197&a_id%5B4%5D=201&a_id%5B5%5D=212&a_id%5B6%5D=214&a_id%5B7%5D=218&a_id%5B8%5D=222&a_id%5B9%5D=224&a_id%5B10%5D=231&a_id%5B11%5D=236&a_id%5B12%5D=249&a_id%5B13%5D=258&a_id%5B14%5D=265&a_id%5B15%5D=287&a_id%5B16%5D=288&a_id%5B17%5D=292&a_id%5B18%5D=293&ignored_agents%5B0%5D=1551&s%5Bmxp%5D=2000&s%5Bmnb%5D=2&s%5Bmnbt%5D=2&s%5Badvanced%5D=1&s%5Bignored_agents%5D%5B0%5D=1551&s%5Bsort_by%5D=date&s%5Bsort_type%5D=d&searchSource=rental'
     const refreshTime = flags.refresh || 1
 
--- src/view/dashboard.ts
+++ src/view/dashboard.ts
@@ -1,41 +1,32 @@
 import {DaftHome} from "../model/daft-home.model";
-
-const HOME_TABLE_OPTS = { keys: true
-  , fg: 'white'
-  , selectedFg: 'white'
-  , selectedBg: 'blue'
-  , interactive: true
-  , label: 'Homes Found'
-  , height: "50%"
-  , width: "100%"
-  , border: {type: "line", fg: "cyan"}
-  , columnSpacing: 10 //in chars
-  , columnWidth: [80, 7, 100] /*in chars*/
-  }
+var blessed = require('blessed')
+var contrib = require('blessed-contrib')
+const open = require('open');
 
 export class Dashboard {
 
+  screen = blessed.screen()
+  grid = new contrib.grid({rows:12, cols: 12, screen: this.screen})
+  table:any = this.grid.set(0, 0, 6, 12, blessed.ListTable, this.createListTable("left", 0, true));
+  markdownLatest = this.grid.set(6, 3, 3, 9, contrib.markdown)
+  markdownSummary = this.grid.set(6,0,3,3, contrib.markdown)
 
   render(homes: DaftHome[]) {
-    var blessed = require('blessed')
-      , contrib = require('blessed-contrib')
-      , screen = blessed.screen()
-      , grid = new contrib.grid({rows:12, cols: 12, screen: screen})
-
-
-    let table = grid.set(0, 0, 6, 12, contrib.table, HOME_TABLE_OPTS);
+    let screen = this.screen;
+    let table = this.table;
     //allow control the table with the keyboard
     table.focus()
     this.generateTable(homes,table)
 
 
+
     // Summary
-    let markdownSummary = grid.set(6,0,3,3, contrib.markdown)
+    let markdownSummary = this.markdownSummary
     markdownSummary.setMarkdown('# Last Update \n '+ new Date().toLocaleString()
      + '\n\n# Total Found \n ' + homes.length)
 
     // Latest Found
-    let markdownLatest = grid.set(6, 3, 3, 9, contrib.markdown)
+    let markdownLatest = this.markdownLatest;
     let latest = homes[0];
     markdownLatest.setMarkdown('# Latest Found \n '
       + '`' +  latest.price+ '`' + ' - ' + latest.title  + '\n '
@@ -44,6 +35,34 @@ export class Dashboard {
 
     screen.key(['escape', 'q', 'C-c'], function(ch:any, key:any) {
       return process.exit(0);
+    });
+
+    // Open article
+    screen.key(["enter"], () => {
+      let selected = homes[table.selected - 1];
+      markdownLatest.setMarkdown('# Latest Found \n '
+        + '`' +  selected.price+ '`' + ' - ' + selected.title  + '\n '
+        + selected.url)
+      screen.render();
+      open(selected.url);
+    });
+
+    screen.key(["up"], () => {
+      let itemToShow = table.selected == 1 ? 0 : table.selected - 2;
+      let selected = homes[itemToShow];
+      markdownLatest.setMarkdown('# Selected \n '
+        + '`' +  selected.price+ '`' + ' - ' + selected.title  + '\n '
+        + selected.url)
+      screen.render();
+    });
+
+    screen.key(["down"], () => {
+      let itemToShow = table.selected == homes.length ? homes.length - 1 : table.selected;
+      let selected = homes[itemToShow];
+      markdownLatest.setMarkdown('# Selected \n '
+        + '`' +  selected.price+ '`' + ' - ' + selected.title  + '\n '
+        + selected.url)
+      screen.render();
     });
 
     screen.on('resize', function() {
@@ -56,12 +75,41 @@ export class Dashboard {
     screen.render()
   }
 
+  createListTable(alignment:string, padding:number = 0, isInteractive:boolean = false) {
+    return {
+      keys: true,
+      align: alignment,
+      selectedFg: "white",
+      selectedBg: "blue",
+      interactive: isInteractive, // Makes the list table scrollable
+      padding: padding,
+      style: {
+          fg: 'white',
+        border: {
+          fg: 'cyan'
+        },
+        cell: {
+          selected: {
+            fg: "black",
+            bg: "light-yellow"
+          }
+        },
+        header: {
+          fg: "red",
+          bold: true
+        }
+      },
+      columnSpacing: 1
+    };
+  }
+
+
   /**
    * Populates the Results Table
    * @param homes
    */
   generateTable(homes: DaftHome[], table:any) {
-    var data:any = []
+    var data:any = [['Title', 'Price', 'URL']]
 
     homes.forEach(home => {
       var row = []
@@ -71,7 +119,7 @@ export class Dashboard {
       data.push(row)
     })
 
-    table.setData({headers: ['Title', 'Price', 'URL'], data: data})
+    table.setData(data)
   }
 
 }
